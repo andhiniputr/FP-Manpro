16182ef8b4a39a6f5829c9382698988c1efb126a
hubedid <hubed@protonmail.com>
2024-11-25 09:04:56 +0700

    add updatestatus bisnis logic

---
+++ updatestatus.php
@@ -0,0 +1,21 @@
+<?php 
+include ('conn.php');
+
+if (isset($_POST['submit'])) {
+    // Periksa apakah ada checkbox yang dipilih
+    if (isset($_POST['taskIds']) && is_array($_POST['taskIds'])) {
+        $taskIds = $_POST['taskIds'];
+
+        // Loop melalui array taskIds untuk mengupdate status tugas
+        foreach ($taskIds as $taskId) {
+            $query = "UPDATE tugas SET status = 1 WHERE ID_Tugas = '$taskId'";
+            mysqli_query(connection(), $query);
+        }
+        
+        header('Location: History.php');
+    } else {
+        echo 'Tidak ada tugas yang dipilih.';
+    }
+}
+
+?>
